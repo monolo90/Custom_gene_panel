25ff848bcc456acfb9214d6d505f7429d9d2d38b
monolo90 <50148768+monolo90@users.noreply.github.com>
2022-01-24 11:17:07 +0100

    Create Custom_panel.R

---
+++ Custom_panel.R
@@ -0,0 +1,34 @@
+GFFtoBED<- function(gtf, assembly_report) {
+
+### SELECT AND MANDE ### GFF
+
+MANE_RefSeqSelect <- read.delim(gtf, header=FALSE, comment.char="#")
+MANE_RefSeqSelect = MANE_RefSeqSelect[MANE_RefSeqSelect$V3=="exon",]
+Refseq_Mane_select = MANE_RefSeqSelect[grepl("RefSeq Select|MANE Select",MANE_RefSeqSelect$V9,),]
+rm(MANE_RefSeqSelect)
+
+
+library(stringr)
+
+information = as.data.frame(str_split_fixed(Refseq_Mane_select$V9, ";", 10))
+###tag= as.data.frame(str_split_fixed(information, "tag ", 2))
+gene= as.data.frame(str_split_fixed(information$V1, "gene_id ", 2))
+trascript= as.data.frame(str_split_fixed(information$V2, "transcript_id", 2))
+exon= as.data.frame(str_split_fixed(Refseq_Mane_select$V9, "exon_number ", 2))
+exon$V2= str_replace(exon$V2, "; ", "")
+name=str_c(gene$V2,exon$V2,sep = "_")
+
+#hg38.p13.chromAlias <- read.delim("~/Downloads/Panel_Roche/hg38.p13.chromAlias.txt", header=FALSE, row.names=NULL, comment.char="#")
+GCF_000001405_39_GRCh38_p13_assembly_report <- read_delim(assembly_report, 
+                                                          delim = "\t", escape_double = FALSE, 
+                                                          col_names = FALSE, comment = "#", trim_ws = TRUE)
+
+
+
+BED_MANE = data.frame(chr=Refseq_Mane_select$V1, start=Refseq_Mane_select$V4, 
+                      end=Refseq_Mane_select$V5, name=name,strand=Refseq_Mane_select$V7 ,gen=gene$V2, exon=exon$V2)
+
+chr= GCF_000001405_39_GRCh38_p13_assembly_report$X10[match(BED_MANE$chr, GCF_000001405_39_GRCh38_p13_assembly_report$X7)]
+BED_MANE$chr = chr
+return(BED_MANE)
+}
